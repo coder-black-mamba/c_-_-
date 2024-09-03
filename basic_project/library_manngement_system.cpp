d9155cc02dd9e013c18cbc33ba2837ced9bdb1b8
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2024-09-03 22:54:33 +0600

    added_basic_mannagement_system

--- basic_project/library_manngement_system.cpp
+++ basic_project/library_manngement_system.cpp
@@ -22,11 +22,11 @@ class Book
 {
 public:
     string name;
-    string id;
+    int id;
     string writter;
     string desc;
     bool is_booked;
-    Book(string n, string i, string w, string d, bool booked)
+    Book(string n, int i, string w, string d, bool booked)
     {
         name = n;
         id = i;
@@ -68,12 +68,35 @@ list<Book> books;
 
 
 */
+void show_avilable_books()
+{
+
+    // Printing Header
+    cout << "------------------------------------------------------------------\n";
+    cout << "     id     " << "|" << "  Name  " << "|" << "  Writter  " << "| " << "  Description  " << "| " << " Status " << "| \n";
+    cout << "------------------------------------------------------------------\n";
+
+    for (Book element : books)
+    {
+        string is_booked = "not booked";
+
+        if (element.is_booked)
+        {
+            is_booked = "booked";
+        }
+
+        cout << element.id << " | " << element.name << " | " << element.writter << " | " << element.desc << " | " << is_booked << "\n";
+    }
+};
 
 int main()
 {
-    Book book_obj_1("Himu", "fdsdf", "Humayen", "Hey What The Fuck I Happening", false);
-    list<Book> books;
-    books.push_back(book_obj_1);
+    // Book book_obj_1("Himu", "fdsdf", "Humayen", "Hey What The Fuck I Happening", false);
+
+    // Book Holder
+    // list<Book> books;
+    // books.push_back(book_obj_1);
+
     // cout << "HEllo";
     // cout<<book_obj_1.name;
     // cout<<books.top();
@@ -96,4 +119,95 @@ int main()
     // void add_book(string names, string writter, string desc, bool booked){
     //     Book book_obj(names, rand(), writter, desc, booked);
     // };
+
+    // fucntion for taking input from the user and save it to a variable
+    // string str_input(){
+
+    // }
+    // Show Avilable Books
+
+    // add book
+    while (true)
+    {
+        cout << "\n\n";
+        cout << "Please Select Options : \n";
+        cout << " |->1. Add A Book \n";
+        cout << " |->2. Show Avilable Books \n";
+        cout << " |->3. Rent A Book \n";
+
+        // User Base Menu Selection Input
+        int user_input;
+        cout << " : ";
+        cin >> user_input;
+
+        // name,id,writter,desc,is_booked
+
+        // checking user input
+        if (user_input == 1)
+        {
+            // cout<<"Add A Book";
+            // Adding A Book
+            try
+            {
+                // name
+                string book_name;
+                cout << "Please Input Book Name : ";
+                cin >> book_name;
+                // writter
+                string writter;
+                cout << "Please Input writter Name : ";
+                cin >> writter;
+
+                // desc
+                string desc;
+                cout << "Please Input Description : ";
+                cin >> desc;
+
+                // is_booked
+                // bool is_booked;
+                // cout << "Please Input writter Name : ";
+                // cin >> writter;
+                // cout<<"\n";
+
+                Book book_obj(book_name, rand(), writter, desc, false);
+                books.push_back(book_obj);
+
+                show_avilable_books();
+            }
+            catch (int v)
+            {
+                cout << "Please Input Correct Data";
+            }
+        }
+        else if (user_input == 2)
+        {
+            cout << "---------------Showing Avilable Books -------------- \n";
+            show_avilable_books();
+        }
+        else if (user_input == 3)
+        {
+            show_avilable_books();
+            cout << "Please Enter The Id Of The Bok You Want To Rent\n";
+            int id;
+            cin >> id;
+
+            int tracker=0;
+            for(Book element:books){
+                tracker++;
+                if (element.id==id and !element.is_booked)
+                {
+                   element.is_booked=true;
+                //    books[tracker].is_booked=true;
+                }
+                
+            }
+
+            cout<<"Book Renting Successfull\n";
+            show_avilable_books();
+        }
+        else
+        {
+            cout << "Please Select Input Between From 1-3 :) \n";
+        }
+    }
 }
